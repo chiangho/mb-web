379ca4d0b9d8410b0d336781dd40d96a72ebf0d4
hao.jiang <hao.jiang@wimisoft.com>
2019-12-21 10:08:26 +0800

    A

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  "plugins": [
+    ["import", { "libraryName": "Antd", "libraryDirectory": "es", "style": "css" }] // `style: true` 会加载 less 文件
   ]
 }
