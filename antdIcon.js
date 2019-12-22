818e8acd221a8ecf231702e9b775282d96c4151c
chiangho <chiang_hao@126.com>
2019-12-23 07:56:05 +0800

    A

--- antdIcon.js
+++ antdIcon.js
@@ -1,6 +1,16 @@
 // 自己项目里面用到的Icon
-export { CheckOutline,
-    CloseCircleOutline,CloseSquareOutline,CloseOutline, DownSquareOutline, LoadingOutline, DownOutline, SearchOutline } from '@ant-design/icons'
+export {
+    CheckOutline,
+    CloseCircleOutline,
+    CloseSquareOutline,
+    CloseOutline,
+    SmileOutline,
+    DownSquareOutline,
+    LoadingOutline,
+    DownOutline,
+    SearchOutline
+}
+from '@ant-design/icons'
 
 //appstore
 //smile
