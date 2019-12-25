5decc251ab2ecd0b36492337fe4b27d97fad977f
hao.jiang <hao.jiang@wimisoft.com>
2019-12-25 17:46:16 +0800

    A

---
+++ src/component/InputNewBook.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    created(){
+        alert(this.type);
+    },
+    props: ["type"],
+}
+</script>
